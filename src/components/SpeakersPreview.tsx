8e3208c8e3c6cb5c08f0ad95857555680b938070
Varshith Reddy <varshithreddy355@gmail.com>
2025-09-15 17:35:32 +0530

    Fixed build errors for the SpeakerPreview

--- src/components/SpeakersPreview.tsx
+++ src/components/SpeakersPreview.tsx
@@ -2,7 +2,7 @@
 
 import SpeakerCard from "./SpeakerCard";
 import Slider from "react-slick";
-import { ChevronLeft, ChevronRight, Youtube } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Settings, CustomArrowProps } from "react-slick";
 import React, { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
@@ -48,8 +48,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/jd_laxmi_narayana.png?updatedAt=1729522774797',
     bio: 'Alice is a visionary leader in innovation and storytelling.',
     description: 'VV (popularly known as) JD Lakshminarayana, a former Joint Director of the Central Bureau of Investigation (CBI) and the current president of the Jai Bharat National Party (JBNP), is a prominent figure in Indian law enforcement and politics. Currently pursuing a Ph.D. in Rural Development. A retired Indian Police Service (IPS) officer from Andhra Pradesh, he has served in key positions, including as the Additional Director General of Police in Mumbai.',
-    expertise: "",
-    social: { instagram: 'https://www.instagram.com/jdlakshminarayana?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==', twitter: 'https://x.com/VVL_Official?t=3S_xt5j8dePVlVmVsf_2wA&s=08', website: '' }
+    expertise: [],
+    social: { 
+      linkedin: '',
+      instagram: 'https://www.instagram.com/jdlakshminarayana?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==', 
+      twitter: 'https://x.com/VVL_Official?t=3S_xt5j8dePVlVmVsf_2wA&s=08', 
+      website: '' 
+    }
   },
   {
     id: 2,
@@ -59,8 +64,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/vanitha_datla.png?updatedAt=1729522782340',
     bio: 'Vice Chairperson & Managing Director of ELICO Ltd.',
     description: 'Vanitha Datla is a highly accomplished professional with nearly three decades of experience in diverse sectors, including financial services, insurance, cement, power, security printing, instrumentation, BPO services, and healthcare in the USA and India. She serves as the Vice Chairperson of Elico and the Managing Director of Elico Healthcare Services Ltd, a Hyderabad-based company. She is a Chartered Financial Analyst and is pursuing a doctoral program at ISB on the Role of Women in Family Businesses. She also serves as treasurer of Women in Network, mentoring children from urban slums to enhance their education and quality of life.',
-    expertise: "['Future Tech', 'Youth Empowerment', 'Technology Education']",
-    social: { linkedin: 'https://www.linkedin.com/in/vanithadatla/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BantXG80GSYyhoQ%2BRvXbfPg%3D%3D', twitter: 'https://twitter.com/vanitha_datla', }
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/vanithadatla/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BantXG80GSYyhoQ%2BRvXbfPg%3D%3D',
+      instagram: '',
+      twitter: 'https://twitter.com/vanitha_datla', 
+      website: ''
+    }
   },
   {
     id: 3,
@@ -70,8 +80,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/dasaraj_centhamli_tarun.png?updatedAt=1729522774024',
     bio: 'MBA graduate from IIM Calcutta DCT Eats food blogger',
     description: 'Dasaraj Centhamil Tarun is the creator of DCT Eats, a rapidly growing food channel with over 1 million subscribers. He holds a B.Com (H) from SRCC and an MBA from IIM Calcutta. DCT initially worked at leading companies like Zomato, Oyo, and Bain. After a brief return to Zomato, he decided to leave corporate life to pursue his passion for food content creation.His channel experienced explosive growth during IPL 2023, gaining 110,000 subscribers in just one month. Today, DCT Eats is celebrated for its engaging and flavorful culinarycontent, attracting food lovers from around the globe.',
-    expertise: "",
-    social: { instagram: 'https://www.instagram.com/dctarun/?utm_source=ig_web_button_share_sheet', Youtube:"https://www.youtube.com/@DCT_EATS" }
+    expertise: [],
+    social: { 
+      linkedin: '',
+      instagram: 'https://www.instagram.com/dctarun/?utm_source=ig_web_button_share_sheet',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 4,
@@ -81,8 +96,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/krithivasam_s.png?updatedAt=1729522774110',
     bio: 'Career Catalyst & HR Innovator.',
     description: 'Krithivasan impressive background highlights his extensive 17+ years of HR experience and leadership in team building and growth initiatives. His roles at Virtusa and Pickyourtrail demonstrate his global reach and strategic acumen in campus strategy and partnerships. As Vice President of HR at YourTribe, he effectively combines his expertise in talent acquisition and HR services with a passion for advising startups. His educational background with an Executive MBA from XLRI Jamshedpur further complements his professional experience. Additionally, his love for travel and commitment to advising academic institutions reflect a well-rounded individual dedicated to fostering innovation and a people-centric HR approach.',
-    expertise: '',
-    social: { linkedin: 'https://www.linkedin.com/in/krithivasans/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BVfIq0H%2FZRu2ooWW76Z2lAg%3D%3D' }
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/krithivasans/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BVfIq0H%2FZRu2ooWW76Z2lAg%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 5,
@@ -92,8 +112,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/BAKHTIAR.png?updatedAt=1729522766052',
     bio: 'Frank pioneers health tech solutions.',
     description: 'Prof. Major Dr. S. Bakhtiar Choudhary (Retd.) is a renowned war veteran and sports medicine specialist with over 40 years of experience, having served both the Indian Army and Apollo Hospitals for a decade each. He has been a visiting faculty member at top institutions, training IPS, IAS, and IRS officers, and advising MNCs and universities. A pioneer in occupational health, he identified and described conditions such as Straight Spine Syndrome and Urban Neck Syndrome. His groundbreaking research has saved thousands from undergoing spinal surgeries. Dr. Choudhary has made significant contributions to sports medicine, advising international athletes, and has been an active promoter of environmental conservation. Additionally, he is a martial artist, an award-winning researcher, an author, and a passionate advocate for ecological education',
-    expertise: "",
-    social: { linkedin: 'https://www.linkedin.com/in/bakhtiar-choudhary-4203b015/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BfJHkPaAPTQOYS%2Beh759S0A%3D%3D'}
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/bakhtiar-choudhary-4203b015/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BfJHkPaAPTQOYS%2Beh759S0A%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 6,
@@ -103,8 +128,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/debabrata_sarkar.png?updatedAt=1729522774116',
     bio: 'Elena is a creative designer and futurist.',
     description: 'Mr. Debabrata Sarkar, a TEDx speaker and accomplished C-level executive, boasts over 26 years of experience in the International Agriculture sector. An IIM Kolkata alumnus, he has held leadership roles at Monsanto, Chemtura, and Syngenta. Currently, he serves as CMD of MicroAlgae Solutions India Pvt Ltd, Director of AGMA Energy, and Vice President - APAC for AE Biologicals. Featured in top publications like Forbes and Fortune, Mr. Sarkar won back-to-back Best Bio Ag CEO Awards in 2022 and 2023, presented by Central Agriculture Minister Mr. Narendra Singh Tomar, recognizing his contributions to sustainable agriculture.',
-    expertise: "",
-    social: { linkedin: 'https://www.linkedin.com/in/debabrata-sarkar-r-20772415/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3ByqHQFwhBQ9iDk16AszGFSA%3D%3D ' }
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/debabrata-sarkar-r-20772415/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3ByqHQFwhBQ9iDk16AszGFSA%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 7,
@@ -114,8 +144,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/hasvi.png?updatedAt=1729522771831',
     bio: 'Passionate Photographer l Cyclist and Runner l Badminton player l National Skater',
     description: 'I am Muriki Pulakita Hasvi, a 16-year-old DexSchool graduate, sports enthusiast, avid cyclist, runner, blogger, MUNer, and TEDx speaker. I have already climbed 3 of the 7 summits and dream of conquering all seven before I turn 19. I hold records as the youngest girl to climb Mt. UT Kangri, Mt. Kang Yatse, and Mt. Dzo Jongo within 70 hours. Additionally, I am the youngest girl to have climbed Mt. Elbrus (the highest mountain in Europe) West and East within 24 hours, as well as Island Peak in Nepal (6,189m). To date, I have completed a total of 12 expeditions. I plan to become a lawyer in the future, as I love reading books and conducting research.',
-    expertise: "",
-    social: { linkedin: 'https://www.linkedin.com/in/hasvi-muriki-9088b5225/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BlYcQwuotREWZEQzHJrY%2Bxg%3D%3D'}
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/hasvi-muriki-9088b5225/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BlYcQwuotREWZEQzHJrY%2Bxg%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 8,
@@ -125,8 +160,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/likitha.png?updatedAt=1729522773211',
     bio: 'Doctor and Miss India AP 2022 Finalist',
     description: 'Likhita Yalamanchili is Miss India Andhra Pradesh 2022 and has also earned the prestigious titles of Times Miss Beauty with a Purpose and Moj Miss Rising Star. She is an MBBS graduate and a successful model, who is now set to make her debut as an actor in the Telugu film industry with her upcoming movie Euphoria, slated for release in early 2025.',
-    expertise: "",
-    social: { instagram: 'https://www.instagram.com/likhita_yalamanchili?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='}
+    expertise: [],
+    social: { 
+      linkedin: '',
+      instagram: 'https://www.instagram.com/likhita_yalamanchili?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 9,
@@ -136,8 +176,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/nikhil_desai.png?updatedAt=1729522775652',
     bio: 'International motivator speaker',
     description: 'Nikhil Desai is dedicated to empowering individuals and organizations to unlock their true potential. Known for his energetic speaking style, he inspires and motivates participants to create lasting change in their lives and work environments. With over 33 years of experience, Nikhil has positively impacted more than a quarter-million participants across 42 countries, including India, the USA, Canada, Sweden, the UAE, and Singapore. His programs are designed to foster personal growth and organizational development. Nikhil has been featured in the media across multiple countries and has appeared on television and radio in India, the USA, the Philippines, South Africa, and Oman, making a global impact with his work.',
-    expertise: "",
-    social: { linkedin: 'https://www.linkedin.com/in/nikhilpdesai/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BCdiy%2BXh0RFKeBgjnHYhcuA%3D%3D', }
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/nikhilpdesai/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BCdiy%2BXh0RFKeBgjnHYhcuA%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 10,
@@ -147,8 +192,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/naveen_samala.png?updatedAt=1729522774666',
     bio: 'Global Portfolio Management Leader-Diebold, Founder-The Guiding Voice',
     description: 'Naveen Samala is a remarkable individual whose journey is a powerful testament to perseverance and the relentless pursuit of dreams. Rising from a modest background, he has become a first-generation post-graduate, showcasing his ambition and dedication. Now a Global Portfolio Manager for Business Transformation, Naveen is also an Udemy instructor, author, career coach, CXO resume writer, guest faculty at BITS Pilani, and podcast visionary. His podcast, The Guiding Voice, ranked in the Global Top 2.5%, is the worlds only Quadrilingual podcast, inspiring millions. With expertise in leadership, productivity, and public speaking, his podcast recently won the Asia Podcast Award for Education, further amplifying his impact globally.',
-    expertise: '',
-    social: { linkedin: 'https://www.linkedin.com/in/naveensamala/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BT4wuo2idQOKI7rUPra%2FnIw%3D%3D'}
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/naveensamala/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BT4wuo2idQOKI7rUPra%2FnIw%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
   {
     id: 11,
@@ -158,8 +208,13 @@ const sampleSpeakers = [
     image: 'https://ik.imagekit.io/tedxvjit/public/speakers/kamal.png?updatedAt=1729522775238',
     bio: 'Kavya transforms digital learning.',
     description: '',
-    expertise: '',
-    social: { linkedin: 'https://www.linkedin.com/in/kraveentharkamal/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BVu4Ve0irR2SJXUdTcw5GyQ%3D%3D' }
+    expertise: [],
+    social: { 
+      linkedin: 'https://www.linkedin.com/in/kraveentharkamal/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BVu4Ve0irR2SJXUdTcw5GyQ%3D%3D',
+      instagram: '',
+      twitter: '',
+      website: ''
+    }
   },
 ];
 
